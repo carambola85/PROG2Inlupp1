0f195a5531117413d3fd270447992e2faf080d0e
Anders Björklund <andersbjorklund@gmail.com>
2012-11-19 14:05:15 +0100

    first test

--- src/grupparbete1/Maraton.java
+++ src/grupparbete1/Maraton.java
@@ -6,29 +6,3 @@ package grupparbete1;
 
 import java.awt.*;
 import javax.swing.*;
-
-/**
- *
- * @author Anders
- */
-class Maraton extends JFrame {
-
-    Maraton(){
-        super("Hej!");
-        setLayout(new FlowLayout());
-        JLabel lab = new JLabel("Namn:");
-        add(lab);
-        JTextField fält = new JTextField(8);
-        add(fält);
-        JButton but = new JButton("Hälsa");
-        add(but);
-        JLabel häls = new JLabel("Hej");
-        add(häls);
-        setSize(230, 100);
-        setVisible(true);
-    }
-
-    public static void main(String[] args) {
-        new Maraton();
-    }
-}
