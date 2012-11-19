87e905720b762d0cdbd61745ae15cff5bdaa5509
Anders Björklund <andersbjorklund@gmail.com>
2012-11-19 16:58:28 +0100

    first test

---
+++ src/Maraton.java
@@ -0,0 +1,22 @@
+
+
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+class Window extends JFrame{
+    Window(){   
+        super("Maratonlöpare");
+     
+        
+        
+        
+        setVisible(true);
+        setSize(300,300);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    public static void main(String[]args){
+    new Window();
+    }
+}
